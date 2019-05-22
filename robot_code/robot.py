3c514f0283e0a2702365207b3b1e7b46d20dd6a2
Zumaad Khan <zumaad@Zumaads-MacBook-Pro.local>
2019-05-22 05:53:50 -0400

    fixed init method as it didn't use the servo_pin argument...

--- robot_code/robot.py
+++ robot_code/robot.py
@@ -10,8 +10,8 @@ SLOW_MAX_PULSE = 1.6/1000
 class Robot:
 
     def __init__(self,servo_pin1 = PWM_PIN1,servo_pin2 = PWM_PIN2, max_pulse = SLOW_MAX_PULSE,min_pulse = SLOW_MIN_PULSE):
-        self.left_servo = Servo(13,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
-        self.right_servo = Servo(18,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
+        self.left_servo = Servo(servo_pin1,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
+        self.right_servo = Servo(servo_pin2,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
         self.command_history = []
         self.recognized_commands = {'ArrowUp':self.move_forward,'ArrowRight':self.turn_right,'ArrowLeft':self.turn_left,'ArrowDown':self.move_backwards}
     
