a66250a0fc30ea8d0e675acbd6df62bfb09973ca
= <khanzumaad@gmail.com>
2019-05-23 17:42:13 -0500

    snytax error.....

--- roach_destroyer_server/robot.py
+++ roach_destroyer_server/robot.py
@@ -12,7 +12,7 @@ class Robot:
 
     def __init__(self,servo_pin1 = PWM_PIN1,servo_pin2 = PWM_PIN2, max_pulse = DEFAULT_MAX_PULSE,min_pulse = DEFAULT_MIN_PULSE):
         self.init_at = datetime.datetime.now().time()
-        self.left_servo = Servo(13,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse
+        self.left_servo = Servo(PWM_PIN1,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
         self.right_servo = Servo(servo_pin2,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
         self.command_history = []
         self.recognized_commands = {'ArrowUp':self.move_forward,'ArrowRight':self.turn_right,'ArrowLeft':self.turn_left,'ArrowDown':self.move_backwards,'stop':self.stop}
