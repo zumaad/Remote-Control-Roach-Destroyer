10b70d99ced9e0d12f6ef483d7cfbc9b7d658bd9
= <khanzumaad@gmail.com>
2019-05-23 05:09:52 -0500

    didn't really do much, weird problem with not bieng able to adjust pulse width without having to recalibrate :(

--- roach_destroyer_server/robot.py
+++ roach_destroyer_server/robot.py
@@ -12,29 +12,29 @@ class Robot:
 
     def __init__(self,servo_pin1 = PWM_PIN1,servo_pin2 = PWM_PIN2, max_pulse = SLOW_MAX_PULSE,min_pulse = SLOW_MIN_PULSE):
         self.init_at = datetime.datetime.now().time()
-        # self.left_servo = Servo(servo_pin1,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
+        self.left_servo = Servo(13,frame_width =20/1000,max_pulse_width = 1.99/1000,min_pulse_width = 1.4/1000)
         self.right_servo = Servo(servo_pin2,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
         self.command_history = []
         self.recognized_commands = {'ArrowUp':self.move_forward,'ArrowRight':self.turn_right,'ArrowLeft':self.turn_left,'ArrowDown':self.move_backwards,'stop':self.stop}
     
     def move_forward(self):
-        # self.left_servo.max()
+        self.left_servo.max()
         self.right_servo.max()
 
     def turn_right(self):
         self.right_servo.max()
-        # self.left_servo.mid()
+        self.left_servo.mid()
 
     def turn_left(self):
-        # self.left_servo.max()
+        self.left_servo.max()
         self.right_servo.mid()
 
     def move_backwards(self):
-        # self.left_servo.min()
+        self.left_servo.min()
         self.right_servo.min()
     
     def stop(self):
-        # self.left_servo.mid()
+        self.left_servo.mid()
         self.right_servo.mid()
 
     def execute_command(self,command):
