5184c4e03c2771467815b14d682f1319ef4b1602
= <khanzumaad@gmail.com>
2019-05-23 17:32:41 -0500

    changed defaults

--- roach_destroyer_server/robot.py
+++ roach_destroyer_server/robot.py
@@ -5,14 +5,14 @@ import datetime
 #CONSTANTS
 PWM_PIN1 = 13
 PWM_PIN2 = 18
-SLOW_MIN_PULSE = 1.4/1000
-SLOW_MAX_PULSE = 1.6/1000
+DEFAULT_MIN_PULSE = 1.4/1000
+DEFAULT_MAX_PULSE = 1.99/1000
 
 class Robot:
 
-    def __init__(self,servo_pin1 = PWM_PIN1,servo_pin2 = PWM_PIN2, max_pulse = SLOW_MAX_PULSE,min_pulse = SLOW_MIN_PULSE):
+    def __init__(self,servo_pin1 = PWM_PIN1,servo_pin2 = PWM_PIN2, max_pulse = DEFAULT_MAX_PULSE,min_pulse = DEFAULT_MIN_PULSE):
         self.init_at = datetime.datetime.now().time()
-        self.left_servo = Servo(13,frame_width =20/1000,max_pulse_width = 1.99/1000,min_pulse_width = 1.4/1000)
+        self.left_servo = Servo(13,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse
         self.right_servo = Servo(servo_pin2,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
         self.command_history = []
         self.recognized_commands = {'ArrowUp':self.move_forward,'ArrowRight':self.turn_right,'ArrowLeft':self.turn_left,'ArrowDown':self.move_backwards,'stop':self.stop}
