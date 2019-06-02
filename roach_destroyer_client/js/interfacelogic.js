097b30ad63536337e25e7d620a13f3d1bad9354d
Zumaad Khan <khanzumaad@gmail.com>
2019-06-01 20:49:14 -0400

    changed streambox

--- roach_destroyer_client/js/interfacelogic.js
+++ roach_destroyer_client/js/interfacelogic.js
@@ -1,4 +1,5 @@
 
+
 class ClientInterface {
     constructor() {
         this.socket = null
@@ -13,22 +14,21 @@ class ClientInterface {
         document.addEventListener('keydown',(event) => this.sendDirection(event))
         document.addEventListener('keyup',(event) => this.stop(event))
         document.getElementById("connectButton").addEventListener('click',() => this.createConnection(this.serverAddress))
-        document.getElementById("speedRange").addEventListener('input',(event) => this.displaySpeed(event))
+        // document.getElementById("speedRange").addEventListener('input',(event) => this.displaySpeed(event))
         document.getElementById('startStream').addEventListener('click',()=> this.startStreamButton())
     }
 
     createConnection(server) {
         this.socket = new WebSocket(server)
-        this.socket.onopen = function (event) {
-            document.getElementById('connectedText').innerHTML = "Succesfully connected to server at " + server; 
-            socket.onmessage = (event) => this.handleServerMessages(event.data)
-        };
+        this.socket.onopen = (event) => document.getElementById('connectedText').innerHTML = "Succesfully connected to server at " + server
+        
+        this.socket.onmessage = (event) => this.handleServerMessages(event.data)
     }
 
     handleServerMessages(data) {
         if (!this.streamBoxReady) {
-            this.clearStreamBox()
             this.streamBoxReady = true;
+            this.clearStreamBox()
             document.getElementById('stream').src = "data:image/jpeg;base64," + data
         }
         else {
@@ -43,7 +43,7 @@ class ClientInterface {
     sendDirection(event) {
         event.preventDefault()
         if (event.key in this.litKeyImageMap && !this.pressed) {
-                socket.send(event.key)
+                this.socket.send(event.key)
                 this.pressed = true
                 this.lightUpArrowDisplay(event.key)      
         }
@@ -51,15 +51,15 @@ class ClientInterface {
 
     stop(event) {
         event.preventDefault()
-        socket.send('stop')
+        this.socket.send('stop')
         document.getElementById(event.key).src = 'assets/' + 'un' + this.litKeyImageMap[event.key]
         this.pressed = false
     }
 
-    displaySpeed(event) {
-        let speedText = document.getElementById("speedText")
-        speedText.innerHTML = event.target.value + '%'
-    }
+    // displaySpeed(event) {
+    //     let speedText = document.getElementById("speedText")
+    //     speedText.innerHTML = event.target.value + '%'
+    // }
 
     startStreamButton() {
         this.socket.send("start stream")
@@ -67,12 +67,18 @@ class ClientInterface {
     }
 
     prepareStreamBox() {
-        let streamBox = document.getElementById('streambox');
+        let streamBox = document.getElementById('streamBox');
         streamBox.removeChild(document.getElementById('startStream'))
+
         let placeholderText = document.createElement('p')
         placeholderText.id = "placeholderText"
         placeholderText.innerHTML = "Waiting for Camera to warm up..."
         streamBox.appendChild(placeholderText)
+
+        let streamImage = document.createElement('img')
+        streamImage.id = 'stream'
+        streamImage.style = 'padding:10px'
+        streamBox.appendChild(streamImage)
     }
 
     clearStreamBox() {
