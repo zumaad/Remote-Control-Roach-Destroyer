e6aad13d53665b37994fa43edfc44597281a6d31
Zumaad Khan <khanzumaad@gmail.com>
2019-06-09 16:32:24 -0400

    huge changes to interface, added ability to view sets of commands

--- roach_destroyer_client_react/src/App.js
+++ roach_destroyer_client_react/src/App.js
@@ -11,41 +11,41 @@ import unlitArrowDown from './assets/unlitdown.png'
 import unlitArrowRight from './assets/unlitright.png'
 import unlitArrowLeft from './assets/unlitleft.png'
 
-const arrowImages = {'litArrowUp':litArrowUp,'litArrowDown':litArrowDown,'litArrowRight':litArrowRight,'litArrowLeft':litArrowLeft,'unlitArrowUp':unlitArrowUp,'unlitArrowDown':unlitArrowDown,'unlitArrowRight':unlitArrowRight,'unlitArrowLeft':unlitArrowLeft}
+const arrowImages = { 'litArrowUp': litArrowUp, 'litArrowDown': litArrowDown, 'litArrowRight': litArrowRight, 'litArrowLeft': litArrowLeft, 'unlitArrowUp': unlitArrowUp, 'unlitArrowDown': unlitArrowDown, 'unlitArrowRight': unlitArrowRight, 'unlitArrowLeft': unlitArrowLeft }
 
 
 
-function ControlPane(props) {
+function ControlPanel(props) {
   return (
-    <div id = "controlsPane">
-        <h3> Movement Controls</h3>
-        <div id = "arrowsPane">
-            <div> <img id = 'ArrowUp' src={arrowImages[props.arrowUp]}/> </div>
-            <div>
-                <img id = 'ArrowLeft' src={arrowImages[props.arrowLeft]}/>
-                <img id = 'ArrowRight' src={arrowImages[props.arrowRight]}/>
-            </div>
-            <div> <img id = 'ArrowDown' src={arrowImages[props.arrowDown]}/> </div>
+    <div id="controlsPane">
+      <h3> Movement Controls</h3>
+      <div id="arrowsPane">
+        <div> <img id='ArrowUp' src={arrowImages[props.arrowUp]} /> </div>
+        <div>
+          <img id='ArrowLeft' src={arrowImages[props.arrowLeft]} />
+          <img id='ArrowRight' src={arrowImages[props.arrowRight]} />
         </div>
+        <div> <img id='ArrowDown' src={arrowImages[props.arrowDown]} /> </div>
+      </div>
     </div>
   )
 }
 
 function Header(props) {
   return (
-    <div id = "header">
-        <h1> Roach Destroyer Client </h1>
+    <div id="header">
+      <h1> Roach Destroyer Client </h1>
     </div>
   )
 }
 
-function ConnectionPane(props) {
-  let commandServerText = props.commandServerConnected? "command server connected": "command server not connected" 
-  let streamServerText = props.streamServerConnected? "streaming server connected" : "streaming server not connected"
+function ConnectionPanel(props) {
+  let commandServerText = props.commandServerConnected ? "command server connected" : "command server not connected"
+  let streamServerText = props.streamServerConnected ? "streaming server connected" : "streaming server not connected"
   return (
-    <div id = "connectionPane">
-        <button onClick = {props.createConnection} className ='btn nightowlButtons' id = "connectButton"> Connect! </button>
-        <p id = "connectedText"> {commandServerText + ' and ' + streamServerText}</p>
+    <div id="connectionPane">
+      <button onClick={props.createConnection} className='btn nightowlButtons' id="connectButton"> Connect! </button>
+      <p id="connectedText"> {commandServerText + ' and ' + streamServerText}</p>
     </div>
   )
 }
@@ -54,43 +54,172 @@ class StreamPanel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      buttonPressed:false
+      buttonPressed: false
     }
     this.prepareStreamBox = this.prepareStreamBox.bind(this);
   }
 
   prepareStreamBox() {
-    this.setState({buttonPressed:true},this.props.startStream)
+    this.setState({ buttonPressed: true }, this.props.startStream)
   }
 
   render() {
-    let button = this.state.buttonPressed? "":<button id = "startStream" className="btn nightowlButtons" onClick = {this.prepareStreamBox}> Start Stream!</button> 
-    let streamImage = this.state.buttonPressed? <img id='stream' style={{padding:'10px'}}/>:""
+    let button = this.state.buttonPressed ? "" : <button id="startStream" className="btn nightowlButtons" onClick={this.prepareStreamBox}> Start Stream!</button>
+    let streamImage = this.state.buttonPressed ? <img id='stream' style={{ padding: '10px' }} /> : ""
     return (
-      <div id ="streamContainer">
-          <div id = "streamBox"> 
-            {button}
-            {streamImage}
-          </div>
+      <div id="streamContainer">
+        <div id="streamBox">
+          {button}
+          {streamImage}
+        </div>
       </div>
     )
-    }
+  }
 }
 
+class HistoryPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      commandSets: 
+      {"name1":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name2":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name3":[{command:'ArrowRight',time:166613},{command:'ArrowLeft',time:66641}],
+      "name4":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name5":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name6":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name7":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name8":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name9":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name10":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name11":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name12":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name13":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name14":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name15":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name16":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name17":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}],
+      "name18":[{command:'ArrowUp',time:123213},{command:'ArrowDown',time:751241}]},
+      currentSet:null,
+      lastTime:null}
+      
+      this.pushSelected = this.pushSelected.bind(this);
+      this.createCommandSets = this.createCommandSets.bind(this);
+  }
+
+  componentWillReceiveProps() {
+    if (this.state.currentSet) {
+      let recievedCommand,recievedTime;
+      [recievedCommand,recievedTime] = this.props.commandAndTime
+      if (recievedTime != this.state.lastTime) {
+        let copiedCommandSet = JSON.parse(JSON.stringify(this.state.commandSets))
+        copiedCommandSet[this.state.currentSet].push({command:recievedCommand,time:recievedTime})
+        this.setState({commandSets:copiedCommandSet,lastTime:recievedTime})
+      }
+      }
+    }
   
+  pushSelected(setName) {
+    this.setState({currentSet:setName})
+  }
+
+  createCommandSets() {
+    let elementArray = []
+    for (let name in this.state.commandSets){
+      elementArray.push(<CommandSet pushSelectedFunc = {this.pushSelected} setName = {name} commands = {this.state.commandSets[name]}/>)
+    }
+    return elementArray
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <HistoryControlsContainer/>
+        <CommandSetContainer createCommandSetsFunc = {this.createCommandSets}/>
+        {this.state.currentSet && <ViewSelectedCommandSet setName = {this.state.currentSet} commandSets = {this.state.commandSets }/>}
+      </div>
+    )
+  }
+}
+
+function CommandSetContainer(props){
+  return (
+    <div id = "commandSetLabelDiv">
+      <h4> <i> Command Sets</i></h4>
+      <div id = "commandSetContainer">
+        {props.createCommandSetsFunc()}
+      </div>
+    </div>
+  )
+}
+
+function HistoryControlsContainer(props){
+  return (
+    <div id = "historyControlsContainer">
+      <div id="historyControlsPanel">
+        <h3> History Controls </h3>
+        <div id="buttonsPane">
+          <button id="historyButton" className="btn nightowlButtons"> Turn history on</button>
+          <button className="btn nightowlErrorButton"> Flush current set</button>
+          <button className="btn nightowlPurpleButton"> New set</button>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
+function CommandSet(props) {
+    return (
+      <div className = "commandSet">
+        <p style = {{fontSize:'1.5em'}}> {props.setName} </p>
+        <div>
+          <button className = "btn nightowlErrorButton"> Delete </button>
+        </div>
+        <div>
+          <button className = "btn nightowlGreenButton"> Play </button>
+        </div>
+        <div>
+          <button> Clear </button>
+        </div>
+        <div>
+          <button onClick = {()=>props.pushSelectedFunc(props.setName)}> Select</button>
+        </div>
+      </div>
+    )
+}
+
+function ViewSelectedCommandSet(props) {
+  return (
+    <div id = "specificSet">
+      <p> {props.setName}</p>
+      {props.commandSets[props.setName].map((command) => <p> {command.command} {command.time} </p>)}
+    </div>
+  )
+
+}
+  
+
+
+
+
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      streamingServer:null,
-      commandServer:null,
-      streamingServerAddress:"ws://192.168.1.3:8764",
+      streamingServer: null,
+      commandServer: null,
+      streamingServerAddress: "ws://192.168.1.3:8764",
       commandServerAddress: "ws://192.168.1.3:8765",
-      commandServerConnected:false,
-      streamServerConnected:false,
-      arrowPressed:false,
-      arrows: {'ArrowUp':'unlitArrowUp','ArrowDown':'unlitArrowDown','ArrowRight':'unlitArrowRight','ArrowLeft':'unlitArrowLeft'}
+      commandServerConnected: false,
+      streamServerConnected: false,
+      arrowPressed: false,
+      arrows: { 'ArrowUp': 'unlitArrowUp', 'ArrowDown': 'unlitArrowDown', 'ArrowRight': 'unlitArrowRight', 'ArrowLeft': 'unlitArrowLeft' },
+      commandAndTime:[]
 
     }
     this.createConnection = this.createConnection.bind(this);
@@ -102,39 +231,41 @@ class App extends React.Component {
 
   createConnection() {
     let commandSocket = new WebSocket(this.state.commandServerAddress)
-    commandSocket.onopen = () => (this.setState({commandServer:commandSocket,commandServerConnected:true}))
-    
+    commandSocket.onopen = () => (this.setState({ commandServer: commandSocket, commandServerConnected: true }))
+
     let streamingSocket = new WebSocket(this.state.streamingServerAddress)
-    streamingSocket.onopen = () => this.setState({streamingServer:streamingSocket,streamServerConnected:true})
-    streamingSocket.onmessage = (event) => this.handleStreamMessages(event.data) 
+    streamingSocket.onopen = () => this.setState({ streamingServer: streamingSocket, streamServerConnected: true })
+    streamingSocket.onmessage = (event) => this.handleStreamMessages(event.data)
   }
 
   sendDirection(event) {
     event.preventDefault();
     if (!this.state.arrowPressed & event.key in this.state.arrows) {
-      this.state.commandServer.send(event.key)
-      this.setState({arrowPressed:true})
-      this.changeArrowDisplay(event.key,'on')
+      // this.state.commandServer.send(event.key)
+      this.setState({ arrowPressed: true ,commandAndTime:[event.key,new Date().getTime()]})
+      this.changeArrowDisplay(event.key, 'on')
+
+
     }
   }
 
   stopMoving(event) {
     if (event.key in this.state.arrows) {
-      this.state.commandServer.send("stop")
-      this.setState({arrowPressed:false})
-      this.changeArrowDisplay(event.key,'off')
-    } 
+      // this.state.commandServer.send("stop")
+      this.setState({ arrowPressed: false })
+      this.changeArrowDisplay(event.key, 'off')
+    }
   }
 
-  changeArrowDisplay(arrowKey,mode) {
+  changeArrowDisplay(arrowKey, mode) {
     let newSources = JSON.parse(JSON.stringify(this.state.arrows));
-    mode === 'on'? newSources[arrowKey] = 'lit' + arrowKey:newSources[arrowKey] = 'unlit' + arrowKey
-    this.setState({arrows:newSources})
+    mode === 'on' ? newSources[arrowKey] = 'lit' + arrowKey : newSources[arrowKey] = 'unlit' + arrowKey
+    this.setState({ arrows: newSources })
   }
 
   handleStreamMessages(data) {
-      document.getElementById('stream').src = "data:image/jpeg;base64," + data
-}
+    document.getElementById('stream').src = "data:image/jpeg;base64," + data
+  }
 
 
   startStream() {
@@ -142,50 +273,33 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state.commandServer)
     return (
-      <div id ="mainPage" onKeyDown = {this.sendDirection} tabIndex="0" onKeyUp = {this.stopMoving}>
+      <div id="mainPage" onKeyDown={this.sendDirection} tabIndex="0" onKeyUp={this.stopMoving}>
+
         <Header />
-        <ConnectionPane 
-          createConnection = {this.createConnection} 
-          commandServerConnected = {this.state.commandServerConnected}
-          streamServerConnected = {this.state.streamServerConnected}
+        <ConnectionPanel
+          createConnection={this.createConnection}
+          commandServerConnected={this.state.commandServerConnected}
+          streamServerConnected={this.state.streamServerConnected}
         />
-            
-      <div id = "controlsAndVideoPane">
-        <ControlPane 
-        arrowUp = {this.state.arrows['ArrowUp']} 
-        arrowDown = {this.state.arrows['ArrowDown']} 
-        arrowLeft = {this.state.arrows['ArrowLeft']}
-        arrowRight = {this.state.arrows['ArrowRight']} />
-        <StreamPanel startStream = {this.startStream} />
-      </div>
-            
-      <div id = "historyPaneContainer">
-        <div id = "historyPane">
-            <h3> History Controls </h3>
-            <div id = "buttonsPane">
-                <button id = "historyButton" className="btn nightowlButtons"> Turn history on</button>
-                <button className="btn nightowlErrorButton"> Flush current set</button>
-                <button className="btn nightowlPurpleButton"> New set</button>
-            </div>
+
+        <div id="controlsAndVideoPane">
+          <ControlPanel
+            arrowUp={this.state.arrows['ArrowUp']}
+            arrowDown={this.state.arrows['ArrowDown']}
+            arrowLeft={this.state.arrows['ArrowLeft']}
+            arrowRight={this.state.arrows['ArrowRight']} />
+          <StreamPanel startStream={this.startStream} />
         </div>
+
+        <HistoryPanel commandAndTime = {this.state.commandAndTime} />
       </div>
-      
-      <div id= "commandsPaneContainer">
-        <div id = "commandsPane">
-          <div>
-              <h4> <i> Commands History</i></h4>
-          </div>
-        </div>
-      </div>
-  </div>
-  
-  
-);
-    }
+
+
+    );
   }
-    
-  
+}
+
+
 
 export default App;
