56b1ed7890371b4853a7917f79c4d5906459c47f
= <khanzumaad@gmail.com>
2019-06-13 21:10:38 -0500

    added playback and reverse to robot

--- roach_destroyer_server/websocketz.py
+++ roach_destroyer_server/websocketz.py
@@ -7,20 +7,21 @@ from picamera import PiCamera
 import base64
 import json
 
-def process_command_set(command_set):
-    return [(command_set[i]["command"],command_set[i+1]["time"] - command_set[i]["time"]) for i in range(0,len(command_set),2)]
 
     
 
 async def main_message_handler(websocket, path):
     print("client connected!")
-    # roach_destroyer = robot.Robot()
+    roach_destroyer = robot.Robot()
     print("roach destroyer initialized!")
+    potential_task = None
     while True:
         message = await websocket.recv()
+        if potential_task:
+            potential_task.cancel()
         deserialized_message = json.loads(message)
         print(deserialized_message)
-        # roach_destroyer.execute_command(deserialized_message)
+        potential_task = roach_destroyer.process_message(deserialized_message)
         
 start_server = websockets.serve(main_message_handler, '0.0.0.0', 8765)
 
--- robot_code/robot.py
+++ robot_code/robot.py
@@ -1,6 +1,8 @@
 
 from gpiozero import Servo
 import datetime
+import asyncio
+import time
 
 
 #CONSTANTS
@@ -15,9 +17,11 @@ class Robot:
         self.init_at = datetime.datetime.now().time()
         self.left_servo = Servo(PWM_PIN1,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
         self.right_servo = Servo(servo_pin2,frame_width =20/1000,max_pulse_width = max_pulse,min_pulse_width = min_pulse)
-        self.recognized_commands = {'ArrowUp':self.move_forward,'ArrowRight':self.turn_right,'ArrowLeft':self.turn_left,'ArrowDown':self.move_backwards,'stop':self.stop,'flush':self.flush_temporary_history}
+        self.command_type_to_method = {'movement':self.execute_movement,'playback':self.return_playback_task,'reverse':self.return_reverse_task}
+        self.movement_commands = {'ArrowUp':self.move_forward,'ArrowRight':self.turn_right,'ArrowLeft':self.turn_left,'ArrowDown':self.move_backwards,'stop':self.stop}
         self.command_database_url = ""
     
+        
     def move_forward(self):
         self.left_servo.max()
         self.right_servo.min()
@@ -39,9 +43,50 @@ class Robot:
         self.left_servo.mid()
         self.right_servo.mid()
 
-        
-    def execute_command(self,command):
-        correct_method = self.recognized_commands[command]
-        correct_method()
+    def return_reverse_task(self,command_set):
+        task = asyncio.ensure_future(self.execute_reverse(command_set))
+        return task
     
-  
+    async def execute_reverse(self,command_set):
+        reversed_directions = {'ArrowUp':'ArrowDown','ArrowDown':'ArrowUp','ArrowRight':'ArrowLeft','ArrowLeft':'ArrowRight'}
+        direction_duration_tups = RobotUtils.process_command_set(command_set)  #[('ArrowRight',3411),('ArrowDown',5000)]
+        direction_duration_tups.reverse()
+        for direction,duration in direction_duration_tups:
+            direction = reversed_directions[direction]
+            print("Executing {} for {} ms".format(direction,duration))
+            self.execute_movement(direction)
+            await asyncio.sleep(duration/1000)
+            self.stop()
+
+
+    def return_playback_task(self,command_set):
+        task = asyncio.ensure_future(self.execute_playback(command_set))
+        return task
+    
+    async def execute_playback(self,command_set):
+        direction_duration_tups = RobotUtils.process_command_set(command_set)  #[('ArrowRight',3411),('ArrowDown',5000)]
+        for direction,duration in direction_duration_tups:
+            print("Executing {} for {} ms".format(direction,duration))
+            self.execute_movement(direction)
+            await asyncio.sleep(duration/1000)
+            self.stop()
+    
+    def execute_movement(self,direction):
+        correct_method = self.movement_commands[direction]
+        correct_method()
+
+        
+    def process_message(self,message):
+        message_type,message_data = message['type'],message['data']
+        correct_method = self.command_type_to_method[message_type] #returns a method
+        return correct_method(message_data)  #this can potentially return a coroutine which can be cancelled in the main loop
+        
+        
+
+    
+
+class RobotUtils:
+
+  @staticmethod
+  def process_command_set(command_set):
+    return [(command_set[i]["command"],command_set[i+1]["time"] - command_set[i]["time"]) for i in range(0,len(command_set),2)]
