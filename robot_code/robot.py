224dd0bbac77ddf863592737094a34c4aa2a1153
Zumaad Khan <khanzumaad@gmail.com>
2019-07-05 20:36:05 -0400

    added state refresh once turn is done

--- robot_code/robot.py
+++ robot_code/robot.py
@@ -42,7 +42,7 @@ class Robot:
         while True:
             for i in chain(range(-90,91),range(90,-91,-1)):
                 self.angular_servo.angle = i
-                await asyncio.sleep(.03) # fastest is like .004
+                await asyncio.sleep(.06) # fastest is like .004
                 angle_and_distance = json.dumps((i,round(self.sonic_sensor.distance * 100,2)))
                 await self.websocket.send(angle_and_distance)
             return 
